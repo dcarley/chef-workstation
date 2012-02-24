c96cf58a34b9362c2607d54cb0bafdf5f60586f5
Dan Carley <dan.carley@gmail.com>
2012-02-24 10:00:18 +0000

    Configure knife
     - Uses same relative path trick for cookbooks.
     - Set my header details for default recipes.
     - Symlink .chef/ so that knife reads it automagically.

---
+++ config/knife.rb
@@ -0,0 +1,8 @@
+root_path = File.expand_path(
+    File.join(File.dirname(__FILE__), "..")
+)
+
+cookbook_path       File.join(root_path, "cookbooks")
+cookbook_copyright  "Dan Carley"
+cookbook_email      "dan.carley@gmail.com"
+cookbook_license    "apachev2"
