99c583b8f1e070d95fa22f43c4533c6ba6cdc016
Dan Carley <dan.carley@gmail.com>
2012-03-08 21:51:52 +0000

    Add template for ntpd.conf

---
+++ cookbooks/ntp/attributes/default.rb
@@ -0,0 +1,6 @@
+default[:ntp][:servers] = [
+    "0.uk.pool.ntp.org",
+    "1.uk.pool.ntp.org",
+    "2.uk.pool.ntp.org",
+    "3.uk.pool.ntp.org"
+]
--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -7,6 +7,14 @@ pkgs.each do |pkg|
     end
 end
 
+template "/etc/#{package_name}/ntpd.conf" do
+    mode 0644
+    owner "root"
+    group "root"
+    source "ntpd.conf.erb"
+    notifies :restart, "service[#{package_name}]"
+end
+
 service package_name do
     action [:enable, :start]
 end
