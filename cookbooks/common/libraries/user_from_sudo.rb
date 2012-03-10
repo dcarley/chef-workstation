fd9122c44690fcd9b1017f23510904428b61b88b
Dan Carley <dan.carley@gmail.com>
2012-03-10 23:35:23 +0000

    Correct variable name REAL_HOME.

--- cookbooks/common/libraries/user_from_sudo.rb
+++ cookbooks/common/libraries/user_from_sudo.rb
@@ -4,6 +4,6 @@ REAL_USER = Etc::getpwuid(Integer(ENV["SUDO_UID"]))[:name]
 REAL_GROUP = Etc::getgrgid(Integer(ENV["SUDO_GID"]))[:name]
 REAL_HOME = ENV['HOME']
 
-if REAL_USER == "root" || REAL_GROUP == "root" || HOME == "/root"
+if REAL_USER == "root" || REAL_GROUP == "root" || REAL_HOME == "/root"
     raise "Unable to determine non-privileged user/group/home"
 end
