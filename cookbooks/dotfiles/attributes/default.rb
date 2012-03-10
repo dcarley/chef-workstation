a2fd978011523f3bd2e5b5f1d27aaaa847f51792
Dan Carley <dan.carley@gmail.com>
2012-03-10 12:22:56 +0000

    Checkout my dotfiles repo.

---
+++ cookbooks/dotfiles/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:dotfiles][:path] = File.join(REAL_HOME, "dotfiles")
+default[:dotfiles][:repo] = "git://github.com/dcarley/dotfiles.git"
--- cookbooks/dotfiles/recipes/default.rb
+++ cookbooks/dotfiles/recipes/default.rb
@@ -1,1 +1,7 @@
-puts "user: #{REAL_USER}"
+git node[:dotfiles][:path] do
+    repository node[:dotfiles][:repo]
+    reference "master"
+    action :sync
+    user REAL_USER
+    group REAL_USER
+end
