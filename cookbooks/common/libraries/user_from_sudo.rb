63a6366ba86e7cd220cdb67c925c2b3d0c647c8a
Dan Carley <dan.carley@gmail.com>
2012-03-13 11:11:36 +0000

    Jiggle some variables around.

--- cookbooks/common/libraries/user_from_sudo.rb
+++ cookbooks/common/libraries/user_from_sudo.rb
@@ -1,8 +1,8 @@
 require 'etc'
 
+REAL_HOME = ENV['HOME']
 REAL_USER = Etc::getpwuid(Integer(ENV["SUDO_UID"]))[:name]
 REAL_GROUP = Etc::getgrgid(Integer(ENV["SUDO_GID"]))[:name]
-REAL_HOME = ENV['HOME']
 
 if REAL_USER == "root" || REAL_GROUP == "root" || REAL_HOME == "/root"
     raise "Unable to determine non-privileged user/group/home"
--- cookbooks/dotfiles/definitions/dotfile_install.rb
+++ cookbooks/dotfiles/definitions/dotfile_install.rb
@@ -2,11 +2,12 @@ define :dotfile_install do
     require 'pathname'
 
     source = File.join(REAL_HOME, params[:name])
-    target = File.join(node[:dotfiles][:path], params[:name])
-    target = Pathname.new(target).relative_path_from(Pathname.new(REAL_HOME)).to_s
     source_exists = File.exists?(source)
 
     log("dotfile '#{source}' does not exist") { level :warn } unless source_exists
+
+    target = File.join(node[:dotfiles][:path], params[:name])
+    target = Pathname.new(target).relative_path_from(Pathname.new(REAL_HOME)).to_s
 
     link source do
         to target
