ccc56dee9721a58404b2f5d325ac7fb41283f53b
Dan Carley <dan.carley@gmail.com>
2012-03-13 15:58:51 +0000

    Confused dotfile source/target. Silly.

--- cookbooks/dotfiles/definitions/dotfile_install.rb
+++ cookbooks/dotfiles/definitions/dotfile_install.rb
@@ -2,17 +2,16 @@ define :dotfile_install do
     require 'pathname'
 
     source = File.join(REAL_HOME, params[:name])
-    source_exists = File.exists?(source)
-
-    log("dotfile '#{source}' does not exist") { level :warn } unless source_exists
-
     target = File.join(node[:dotfiles][:path], params[:name])
-    target = Pathname.new(target).relative_path_from(Pathname.new(REAL_HOME)).to_s
+    target_exists = File.exists?(target)
+    target_relative = Pathname.new(target).relative_path_from(Pathname.new(REAL_HOME)).to_s
+
+    log("dotfile '#{target}' does not exist") { level :warn } unless target_exists
 
     link source do
         to target
         owner REAL_USER
         group REAL_GROUP
-        only_if { source_exists }
+        only_if { target_exists }
     end
 end
