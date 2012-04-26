f578176e81b6ce2124d3b6ba6cdd1ec003ac67e3
Dan Carley <dan.carley@gmail.com>
2012-04-26 11:11:28 +0100

    rbenv-bundler was incorrectly named.

--- cookbooks/rbenv/attributes/plugins.rb
+++ cookbooks/rbenv/attributes/plugins.rb
@@ -1,4 +1,4 @@
 default[:rbenv][:plugins] = {
-    "rbenv-builder" => "git://github.com/carsomyr/rbenv-bundler.git",
+    "rbenv-bundler" => "git://github.com/carsomyr/rbenv-bundler.git",
     "rbenv-sudo"    => "git://github.com/dcarley/rbenv-sudo.git",
 }
