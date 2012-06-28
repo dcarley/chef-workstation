95657c59f0eb1b0b1f62a6b6ef9ef7f7152cd75d
Dan Carley <dan.carley@gmail.com>
2012-06-28 17:56:25 +0100

    Create parent dotfiles dirs if they don't exist
    Prevents errors such as ~/.gconf sub-dirs not existing.

--- cookbooks/dotfiles/definitions/dotfile_install.rb
+++ cookbooks/dotfiles/definitions/dotfile_install.rb
@@ -2,14 +2,22 @@ define :dotfile_install do
     require 'pathname'
 
     source = File.join(REAL_HOME, params[:name])
+    source_parent = Pathname.new(source).parent
+
     target = File.join(node[:dotfiles][:path], params[:name])
     target_exists = File.exists?(target)
-    target_relative = Pathname.new(target).relative_path_from(Pathname.new(source).parent).to_s
+    target_relative = Pathname.new(target).relative_path_from(source_parent)
 
     log("dotfile '#{target}' does not exist") { level :warn } unless target_exists
 
+    directory source_parent.to_s do
+        owner REAL_USER
+        group REAL_GROUP
+        recursive true
+    end
+
     link source do
-        to target_relative
+        to target_relative.to_s
         owner REAL_USER
         group REAL_GROUP
         only_if { target_exists }
